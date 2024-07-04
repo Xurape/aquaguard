4ddacd5997584162430110c068e660fb22dcd79c
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-07-04 11:56:52 +0100

    add: manual lat and long

--- app/Modules/EnvironmentMetrics/Http/Controllers/EnvironmentMetricsController.php
+++ app/Modules/EnvironmentMetrics/Http/Controllers/EnvironmentMetricsController.php
@@ -7,13 +7,6 @@ use Illuminate\Http\Request;
 
 class EnvironmentMetricsController extends Controller
 {
-    private $API_KEY;
-
-    public function __construct()
-    {
-        $this->API_KEY = env('IPGEOLOCATION_IO_API_KEY');
-    }
-
     protected function getData($lat, $long) {
         $url = "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$long&current=temperature_2m,is_day,precipitation,wind_speed_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,wind_speed_10m,wind_gusts_10m&forecast_days=1";
         $ch = curl_init();
@@ -26,24 +19,15 @@ class EnvironmentMetricsController extends Controller
 
     public function temperature()
     {
-        $api = $this->API_KEY;
-        $url = "https://api.ipgeolocation.io/ipgeo?apiKey=$api";
+        $lat = env('APP_LATITUDE');
+        $long = env('APP_LONGITUDE');
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        $geo = json_decode($output, true);
-
-        $lat = $geo["latitude"];
-        $long = $geo["longitude"];
-
-        $data = $this->getData($lat, $long)->current;
+        $data = $this->getData($lat, $long);
 
         return response()->json([
-            'temperature' => $data->temperature_2m,
-            'is_day' => $data->is_day,
+            'temperature' => $data->current->temperature_2m,
+            'is_day' => $data->current->is_day,
+            'precipitation' => $data->current->precipitation,
         ]);
     }
 
